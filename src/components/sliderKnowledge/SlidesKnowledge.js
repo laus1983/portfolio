37e878314c7d7b8f4c9c4b38f18931b5bf1d587d
Luis Urdaneta <luisurdaneta1983@gmail.com>
2022-10-06 21:25:46 -0400

    Certificados Agregados

--- src/components/sliderKnowledge/SlidesKnowledge.js
+++ src/components/sliderKnowledge/SlidesKnowledge.js
@@ -47,6 +47,51 @@ const slidesInfo = [
     alt: "Certificado EF SET",
     desc: "Nivel B1 Inglés",
   },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103827/Certificados%20Dev/LuisUrdaneta-PCAP_Programming-certificate_page-0001_abho6n.jpg",
+    alt: "Certificado PCAP Programming Essentials in Python ",
+    desc: "PCAP: Programming Essentials in Python",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103827/Certificados%20Dev/LuisUrdaneta-Intro-Cibersecur-certificate_page-0001_s538yu.jpg",
+    alt: "Certificado Introducción a la Seguridad Cibernética",
+    desc: "Introducción a la Seguridad Cibernética",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103826/Certificados%20Dev/LuisUrdaneta-Introduction_to_-certificate_page-0001_rdcv73.jpg",
+    alt: "Certificado Introducción a IoT",
+    desc: "Introducción a IoT",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103826/Certificados%20Dev/Certificado_Curso_Programacion_con_JavaScript_page-0001_jcsqtt.jpg",
+    alt: "Certificado Programación con JavaScript",
+    desc: "Programación con JavaScript",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103826/Certificados%20Dev/Certificado_Curso_Desarrollo_de_Servicios_en_la_Nube_page-0001_pm0ya6.jpg",
+    alt: "Certificado Desarrollo de Servicios en la Nube",
+    desc: "Desarrollo de Servicios en la Nube",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103826/Certificados%20Dev/Certificado_Git_y_GitHub_page-0001_xz21vk.jpg",
+    alt: "Certificado Git y GitHub",
+    desc: "Git y GitHub",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665103825/Certificados%20Dev/Certificado_Curso_Programacion_con_Java_Standard_page-0001_wbippd.jpg",
+    alt: "Certificado Programación con Java Standard",
+    desc: "Programación con Java Standard",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665105714/Certificados%20Dev/Applied_Data_Science_I_azqlus.png",
+    alt: "Certificado Applied Data Science I",
+    desc: "Applied Data Science I",
+  },
+  {
+    src: "https://res.cloudinary.com/dzoshddvt/image/upload/v1665105714/Certificados%20Dev/Applied_Data_Science_II_opzifj.png",
+    alt: "Certificado Applied Data Science II",
+    desc: "Applied Data Science II",
+  },
 ];
 
 const SlidesKnowledge = slidesInfo.map((slide) => (
